642ba84ae1909cd849cdc8faf999ba3d07d5ba09
dimab <dimabelov2@icloud.com>
2023-08-23 18:21:28 +0300

    site

--- сайты/4_les/myproject/app_myproject/models.py
+++ сайты/4_les/myproject/app_myproject/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Advertisements(models.Model):
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("Торг", help_text="Отметьте, если будет уместен торг")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    db_table = 'advertisements'
+    def __str__(self): 
+        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
